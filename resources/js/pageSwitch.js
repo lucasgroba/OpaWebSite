871609a3feacdf4f939463eb81c9b36a8f12ba2d
LucasGroba <lucasgroba9@hotmail.com>
2017-09-04 14:57:12 -0300

    modificaciones de css y arbol de categorias

--- resources/js/pageSwitch.js
+++ resources/js/pageSwitch.js
@@ -4,11 +4,18 @@ var switchContent = function (page) {
     $.ajax({
         url: page,
     }).done(function (data) {
-        $(".container").html(data);
+        $(".containerD").html(data);
+    });
+};
+var switchTreeContent = function (page) {
+    $.ajax({
+        url: page,
+    }).done(function (data) {
+        $(".container-tree").html(data);
     });
 };
 
-
 switchContent('templates/deals/dealList.html');
+switchTreeContent('templates/categories/tree_Categories.html');
 
 
