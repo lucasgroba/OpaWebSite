23e5b873a29b19b70e9a1670249435f69bbe8af8
Joaquín <laguardaj@gmail.com>
2017-09-05 16:25:45 -0300

    Revert "."
    This reverts commit 9ce3354e7b8cc46f209391a1dfef332ba9e53022.

--- resources/js/DealsDetails.js
+++ resources/js/DealsDetails.js
@@ -1,10 +1,19 @@
+var offersDeals = [{
+      "Id": 1,
+      "Name": "Lorem ipsum dolor sit amet.",
+      "providerId": 1,
+      "rating": 3.5,
+      "price": 1500,
+      "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur eget.",
+      "longDescription": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin nibh augue, congue blandit magna nec, sagittis iaculis metus. In pulvinar consectetur diam, eget tristique risus tincidunt id. Suspendisse nec tincidunt arcu. Ut mattis lorem ut varius condimentum. Nunc at elementum leo. Nullam ut congue leo, ultrices efficitur risus. Ut vestibulum nisl in accumsan convallis. Nunc metus est, efficitur ut tristique.",
+      "Details": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin nibh augue, congue blandit magna nec, sagittis iaculis metus. In pulvinar consectetur diam, eget tristique risus tincidunt id. Suspendisse nec tincidunt arcu. Ut mattis lorem ut varius condimentum. Nunc at elementum leo. Nullam ut congue leo, ultrices efficitur risus. Ut vestibulum nisl in accumsan convallis. Nunc metus est, efficitur ut tristique.",
+      "sellConditions": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin nibh augue, congue blandit magna nec, sagittis iaculis metus. In pulvinar consectetur diam, eget tristique risus tincidunt id. Suspendisse nec tincidunt arcu. Ut mattis lorem ut varius condimentum. Nunc at elementum leo. Nullam ut congue leo, ultrices efficitur risus. Ut vestibulum nisl in accumsan convallis. Nunc metus est, efficitur ut tristique."
+}];
 
-function getDetails(){
-      var deal = read("/deals", LoadOfferDatails);
-}
-var LoadOfferDatails = function (offersDeals){
+function LoadOfferDatails(){
       document.getElementsByClassName('Deals-Details-Name')[0].innerText = offersDeals[0].Name;
-      document.getElementsByClassName('Deals-Details-Price')[0].innerText = "Precio: $"+offersDeals[0].price;
+      document.getElementsByClassName('Deals-Details-Price')[0].innerText = offersDeals[0].price;
       document.getElementsByClassName('Deals-Details-Description')[0].innerText = offersDeals[0].description;
 }
-getDetails();
+
+LoadOfferDetails();
--- resources/js/pageSwitch.js
+++ resources/js/pageSwitch.js
@@ -4,20 +4,11 @@ var switchContent = function (page) {
     $.ajax({
         url: page,
     }).done(function (data) {
-        $(".containerD").html(data);
+        $(".container").html(data);
     });
 };
 
-
-var switchTreeContent = function (page) {
-    $.ajax({
-        url: page,
-    }).done(function (data) {
-        $(".container-tree").html(data);
-    });
-};
 
 switchContent('templates/deals/dealList.html');
-switchTreeContent('templates/categories/tree_Categories.html');
 
 
